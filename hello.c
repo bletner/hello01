71bd918343a149739f2f0249aaef2ca15f38f1a3
Brevin Letner <bletner2@huskers.unl.edu>
2020-08-19 14:20:10 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+* Author: Brevin Letner
+* Date: 08/19/2020
+*
+* Hack 01 - Hello World
+*/
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char **argv) {
+
+  printf("Brevin Letner"); //Print Name to Screen
+
+  return 0;
+}
